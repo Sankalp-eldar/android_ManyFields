5a4f03d0bd0e4c74c88ae43145d4a3ff70d9ef84
Sankalp <ssankalp@gmail.com>
2024-02-29 02:17:27 +0530

    Added all needed functionality to the app. Completed

--- app/src/main/java/com/example/manyfield/GreetUser.java
+++ app/src/main/java/com/example/manyfield/GreetUser.java
@@ -1,5 +1,6 @@
 package com.example.manyfield;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.w3c.dom.Text;
 
 public class GreetUser extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +19,16 @@ public class GreetUser extends AppCompatActivity {
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("Name");
-//        TextView txt = (TextView) findViewById(R.id.textView2);
-//        txt.setText("Hello, " + str);
+        TextView txt = (TextView) findViewById(R.id.personal);
+        txt.setText("Hello, " + str);
+
+        txt = findViewById(R.id.gender);
+        txt.setText("Gender: "+intent.getStringExtra("Gender"));
+
+        txt = findViewById(R.id.tandc);
+        txt.setText("T&C selected: "+ intent.getBooleanExtra("T&C", false));
+
+        txt = findViewById(R.id.mails);
+        txt.setText("Promotions selected: "+intent.getBooleanExtra("Mails", true));
     }
 }
--- app/src/main/java/com/example/manyfield/MainActivity.java
+++ app/src/main/java/com/example/manyfield/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,25 +18,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     EditText ent_name;
-    Button btn_next;
+    Button btn_next, btn_clear;
+    CheckBox t_and_c, mails;
+    RadioGroup rdo_group;
+    String rdo_selection = "Nil";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText ent_name = findViewById(R.id.editTextText);
-        Button btn = findViewById(R.id.button);
+        ent_name = findViewById(R.id.editTextText);
+        btn_next = findViewById(R.id.btn_next);
+        btn_clear = findViewById(R.id.btn_clear);
+        t_and_c = findViewById(R.id.checkBox);
+        mails = findViewById(R.id.checkBox2);
+        rdo_group = (RadioGroup) findViewById(R.id.rdo_gender);
+
+        rdo_group.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == -1) {
+                rdo_selection = "Nil";
+                return;
+            }
+            RadioButton btn = (RadioButton) findViewById(checkedId);
+            rdo_selection = btn.getText().toString();
+        });
     }
     public void onClickNext(View view) {
-        Button btn = findViewById(R.id.button);
-        EditText text = findViewById(R.id.editTextText);
-        String str = text.getText().toString() ;
+        String str = ent_name.getText().toString() ;
 
         Intent intent = new Intent(this, GreetUser.class);
         intent.putExtra("Name", str);
+        intent.putExtra("T&C", t_and_c.isChecked());
+        intent.putExtra("Mails", mails.isChecked());
+        intent.putExtra("Gender", rdo_selection);
         startActivity(intent);
     }
     public void onClickClear(View v){
-
+        rdo_group.clearCheck();
+        ent_name.setText("");
+        t_and_c.setChecked(false);
+        mails.setChecked(false);
     }
 }
